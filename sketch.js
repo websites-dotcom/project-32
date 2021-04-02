612b66f3f6ea08edef6cb365a780ee9f3f887f90
websites-dotcom <0205.akshatgupta@gmail.com>
2021-04-02 16:14:44 +0530

    Added all of the required files

---
+++ sketch.js
@@ -0,0 +1,48 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+
+var bg ;
+
+function preload() {
+    // create getBackgroundImg( ) here
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+
+    // add condition to check if any background image is there to add
+
+
+    Engine.update(engine);
+
+    // write code to display time in correct format here
+
+
+}
+
+async function getBackgroundImg(){
+
+    // write code to fetch time from API
+
+    //change the data in JSON format
+
+    // write code slice the datetime
+
+
+    // add conditions to change the background images from sunrise to sunset
+
+
+    //load the image in backgroundImg variable here
+
+}
